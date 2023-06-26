d9d8605207cb8b8e882da18f50598ec3379c5d5a
Linda Paz <lindacsndelapaz@gmail.com>
2023-06-26 11:56:59 -0300

    add router-outlet to itr page

--- src/app/itr/itr/itr-routing.module.ts
+++ src/app/itr/itr/itr-routing.module.ts
@@ -3,19 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 // Layout
 import { AsignadorComponent, CoachComponent, DepartamentoComponent, EntrevistadorComponent, InicioComponent, LayoutPageComponent, RecursoComponent, RrhhComponent } from '../pages';
 
-// localhost:4200/itr
+// localhost:4200/itr/asignador
 const routes: Routes = [
   {
     path: '',
     component: LayoutPageComponent,
     children: [
-      {path:'asignador', component: AsignadorComponent}, // asugnador
+      {path:'asignador', component: AsignadorComponent}, // asignador
       {path:'coach', component: CoachComponent}, // coach
       {path:'departamento', component: DepartamentoComponent}, // departamento
       {path:'entrevistador', component: EntrevistadorComponent}, // entrevistador
       {path:'inicio', component: InicioComponent}, // inicio
       {path:'recurso', component: RecursoComponent}, // recurso
       {path:'rrhh', component: RrhhComponent}, // rrhh
+      {path:'**', redirectTo:'inicio'}, // inicio
     ]
   }
 ];
