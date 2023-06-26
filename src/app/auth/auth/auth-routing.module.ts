512e4c1d642e2ae85e2c5d84a220dc88b9be1728
Linda Paz <lindacsndelapaz@gmail.com>
2023-06-26 11:39:46 -0300

    add auth-routing

--- src/app/auth/auth/auth-routing.module.ts
+++ src/app/auth/auth/auth-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Layout
+import { LayoutComponent, LoginComponent, RegisterComponent } from '../pages';
+
+// localhost:4200/auth
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent, // mostrando en el app
+    children: [
+      {path: 'login', component: LoginComponent}, // localhost:4200/auth/login
+      {path: 'register', component: RegisterComponent}, // localhost:4200/auth/register
+      {path: '**', redirectTo: 'login'} // comodin
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
