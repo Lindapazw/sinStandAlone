1b4094045ab4167d17fb3622bd9c49080c8741a5
Linda Paz <lindacsndelapaz@gmail.com>
2023-06-26 15:31:28 -0300

    add browser module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,7 +34,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
