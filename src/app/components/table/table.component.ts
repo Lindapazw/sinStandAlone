70c78b23dd9a1f2ae42dec8f71adb983a228e381
Linda Paz <lindacsndelapaz@gmail.com>
2023-06-27 12:16:30 -0300

    add table component

---
+++ src/app/components/table/table.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-table',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.css']
+})
+export class TableComponent {
+
+}
