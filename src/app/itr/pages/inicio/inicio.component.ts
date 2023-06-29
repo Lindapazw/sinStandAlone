99c0f2b21388f40f25a3f066b69dbc8bf4533fc7
Linda Paz <lindacsndelapaz@gmail.com>
2023-06-29 10:54:18 -0300

    add tips to inicio

--- src/app/itr/pages/inicio/inicio.component.ts
+++ src/app/itr/pages/inicio/inicio.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { NgbModal, NgbModalOptions } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css']
 })
-export class InicioComponent {
 
+export class InicioComponent {
+  constructor(public modalService: NgbModal) { }
+
+  openModal(content: any) {
+    const modalOptions: NgbModalOptions = {
+      scrollable: true
+    };
+
+    this.modalService.open(content, modalOptions);
+  }
 }
