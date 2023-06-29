eab3bcd3bafcfe4a7c52b606ccd25e40d58a9c1a
Ludmila Alloatti <ludmila_alloatti@itrsa.com.ar>
2023-06-29 14:14:23 -0300

    Logica filtro

--- src/app/itr/pages/coach/coach.component.ts
+++ src/app/itr/pages/coach/coach.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgIterable, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 import { CoachDTO } from 'src/app/model/CoachDTO';
 import { CoachService } from 'src/app/service/coach.service';
 
@@ -8,13 +9,25 @@ import { CoachService } from 'src/app/service/coach.service';
   styleUrls: ['./coach.component.css'],
 })
 export class CoachComponent implements OnInit {
-
   constructor(private coachService: CoachService) {}
+ 
+  dataSource: CoachDTO[] = [];
+  filterCoach: CoachDTO[] = [];
 
-  dataSource: CoachDTO[]
-  
   ngOnInit(): void {
-    this.coachService.listCoaches()
-    .then( response => {this.dataSource = response});
+    this.coachService.listCoaches().then((response) => {
+      this.dataSource = response;
+      this.filterCoach = response;
+    });
+  }
+
+  // Filtro para coach-card
+  applyFilter(event: any) {
+    const filterValue = event.target.value.toLowerCase();
+
+    this.filterCoach = this.dataSource.filter((coach: CoachDTO) => {
+      const fullName = coach.name.toLowerCase() + ' ' + coach.lastname.toLowerCase();
+      return fullName.includes(filterValue);
+    });
   }
 }
