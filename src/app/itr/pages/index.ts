4d25f92412f7129d1c0ec7bf2b9407bc2ca1c7a9
Marina Celeste Chavez <94614751+marinacchavez89@users.noreply.github.com>
2023-06-30 10:05:14 -0300

    delete dpto y entrevistador, correccion de errores

--- src/app/itr/pages/index.ts
+++ src/app/itr/pages/index.ts
@@ -1,7 +1,5 @@
 export * from './asignador/asignador.component';
 export * from './coach/coach.component';
-export * from './departamento/departamento.component';
-export * from './entrevistador/entrevistador.component';
 export * from './inicio/inicio.component';
 export * from './layout-page/layout-page.component';
 export * from './recurso/recurso.component';
